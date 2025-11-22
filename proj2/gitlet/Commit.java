77d1c2d14f1044f2c807ff44f3415e4b9d7888e4
MichaelHart23 <handongxiu23@gmail.com>
2025-11-22 21:33:40 +0800

    gitlet init command done

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,15 +2,16 @@ package gitlet;
 
 // TODO: any imports you need here
 
+import java.io.Serializable; 
 import java.util.Date; // TODO: You'll likely use this in this class
-
+import java.util.HashMap;
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author Michael Hart
  */
-public class Commit {
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -21,6 +22,27 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
+    /*bolbs的哈希值和文件名的键值对 */
+    private HashMap<String, String> map;
+    /*父commit的id */
+    private String parent;
+    private String secondParent;
+    /*时间戳 */
+    private Date timeStamp;
+    /*自身的id */
+    private String id;
 
-    /* TODO: fill in the rest of this class. */
+    public Commit() {}
+    public static Commit initialCommit() {
+        Commit commit = new Commit();
+        commit.message = "initial commit";
+        commit.timeStamp = new Date(0);
+        commit.id = Utils.sha1(commit.message, commit.timeStamp.toString());
+        return commit;
+    }
+    public static void saveCommit(Commit commit) {}
+
+    public String getID() {
+        return id;
+    }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,7 +1,9 @@
 package gitlet;
 
 import java.io.File;
-import static gitlet.Utils.*;
+import java.io.IOException;
+
+import static gitlet.Utils.join;
 
 // TODO: any imports you need here
 
@@ -9,7 +11,7 @@ import static gitlet.Utils.*;
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author Michael Hart
  */
 public class Repository {
     /**
@@ -25,5 +27,68 @@ public class Repository {
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
-    /* TODO: fill in the rest of this class. */
+    /*存放所有blobs和commits的目录 */
+    public static final File OBJECTS = join(GITLET_DIR, "objects");
+    /*存放所有分支的目录，每个以分支名命名的文件中存的是head commit的哈希值 */
+    public static final File BRACNCHES = join(GITLET_DIR, "branches");
+    /*当前的commit head是哪个分支，存的是分支名 */
+    public static final File HEAD = join(GITLET_DIR, "HEAD");
+    /*暂存区 用来存放要添加的文件，要修改的文件，要删除的文件*/
+    public static final File STAGE = join(GITLET_DIR, "stage");
+
+    private static void setupPersistence() {
+        if(GITLET_DIR.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+        }
+        GITLET_DIR.mkdir();
+        OBJECTS.mkdir();
+        BRACNCHES.mkdir();
+        try {
+            HEAD.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Error creating file: " + e.getMessage());
+        }
+
+        try {
+            STAGE.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Error creating file: " + e.getMessage());
+        }
+    }
+
+    private static void zeroCommit(Commit commit) {
+        File f = join(OBJECTS, commit.getID());
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Error creating file: " + e.getMessage());
+        }
+        Utils.writeObject(f, commit);
+    }
+
+    private static void makeMasterBranch(String id) {
+        File master = join(BRACNCHES, "master");
+        try {
+            master.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Error creating file: " + e.getMessage());
+        }
+        Utils.writeContents(master, id);
+        Utils.writeContents(HEAD, "master"); //初始commit为master分支的commit head
+    }
+
+    public static void init() { //仅在gitlet init时被调用
+        setupPersistence();
+        Commit initialCommit = Commit.initialCommit();
+        zeroCommit(initialCommit);
+        makeMasterBranch(initialCommit.getID());
+    }
+
+    public void newBranch(String branchName) { //to be finshed
+        File f = join(BRACNCHES, branchName);
+        if(f.exists()) {
+            System.out.println("A branch with that name already exists.");
+            System.exit(1);
+        }
+    }
 }
