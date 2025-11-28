79e8f93f39963ba137ff2fb98641564a9c73a472
MichaelHart23 <handongxiu23@gmail.com>
2025-11-28 22:01:54 +0800

    style

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please enter a command.");
-            System.exit(-1);
+            System.exit(0);
         }
         String firstArg = args[0];
         switch(firstArg) {
